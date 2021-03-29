4383b3788fa232a5bd957d5d24cb50518d48a1a5
Ilyar <ilyar.yusupov52@gmail.com>
2021-03-29 15:21:23 +0600

    Added DI container

--- api/public/index.php
+++ api/public/index.php
@@ -2,15 +2,31 @@
 
 declare(strict_types=1);
 
+use DI\Container;
+use DI\ContainerBuilder;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Slim\Factory\AppFactory;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-$app = \Slim\Factory\AppFactory::create();
+$builder = new ContainerBuilder();
+
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ]
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
 
 $app->get('/', function (RequestInterface $request, ResponseInterface $response, $args) {
     $response->getBody()->write('{}');
+    //throw new Exception('eye');
     return $response->withHeader('Content-Type', 'application/json');
 });
 
