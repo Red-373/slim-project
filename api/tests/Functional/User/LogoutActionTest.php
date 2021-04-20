92b50cfbaadbc1cfc11173f6a186eb974801506d
Ilyar <ilyar.yusupov52@gmail.com>
2021-04-20 19:02:45 +0600

    Logout tests.

---
+++ api/tests/Functional/User/LogoutActionTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional\User;
+
+use Test\Functional\WebTestCase;
+
+class LogoutActionTest extends WebTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testSuccess(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/v1/user/logout', [], self::$HEADERS));
+        $data = json_decode((string)$response->getBody(), true);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals([], $data);
+    }
+}
